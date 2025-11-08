2c44befc8f017dca64372fcd9d78d95b05876ee4
saloni jethwa <jethwa14saloni@gmail.com>
2025-11-08 10:14:18 +0100

    hyperparamter tranning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,11 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainer
 
-a =100
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -33,8 +36,8 @@ class DataIngestion:
             logging.info("Saved the raw data to artifacts folder")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Ingestion of the data is completed")
 
@@ -49,4 +52,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion
+    train_data, test_data = obj.initiate_data_ingestion
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    r2_score = model_trainer.initiate_model_trainer(train_arr, test_arr)
+    print("R2 Score :", r2_score)
---
+++ src/utils.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import dill
+
+from src.exception import CustomException
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
+
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise Exception(f"Error saving object to {file_path}: {e}")
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    model_report = {}
+    try:
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            #model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            r2_square = r2_score(y_test, y_test_pred)
+            r2_square = r2_score(y_train, y_train_pred)
+            model_report[list(models.keys())[i]] = r2_square
+        return model_report
+    except Exception as e:
+        raise CustomException(e, sys)
